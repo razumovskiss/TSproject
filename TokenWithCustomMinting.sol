7382da806dfcf87f5bbfd4cf58266c8e6abbee0d
sarcazm <nomerchik@gmail.com>
2025-01-29 14:04:22 +0300

    TokenWithCustomMinting.sol

--- TokenWithCustomMinting.sol
+++ TokenWithCustomMinting.sol
@@ -5,18 +5,18 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contr
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/access/Ownable.sol";
 
 contract TokenWithCustomMinting is ERC20, Ownable {
-    uint256 public maxSupply; // Максимальное количество токенов, которое можно выпустить
-    uint256 public tokenPrice; // Цена за токен в wei
-    address public conditionChecker; // Адрес контракта/сервиса для проверки внешнего условия
+    uint256 public maxSupply; 
+    uint256 public tokenPrice; 
+    address public conditionChecker; 
 
     event TokensMinted(address indexed to, uint256 amount);
 
     constructor(
-    string memory name,       // Имя токена (например, "MyToken")
-    string memory symbol,     // Символ токена (например, "MTK")
-    uint256 initialSupply,    // Начальное количество токенов (например, 1000 * 10**18)
-    uint256 _maxSupply,       // Максимальное количество токенов (например, 1000000 * 10**18)
-    uint256 _tokenPrice       // Цена токена в wei (например, 1000000000000000 = 0.001 ETH)
+    string memory name,       
+    string memory symbol,    
+    uint256 initialSupply,    
+    uint256 _maxSupply,
+    uint256 _tokenPrice
     ) ERC20(name, symbol) {
         require(_maxSupply > initialSupply, "Max supply must be greater than initial supply");
         _mint(msg.sender, initialSupply);
@@ -24,12 +24,10 @@ contract TokenWithCustomMinting is ERC20, Ownable {
         tokenPrice = _tokenPrice;
     }
 
-    // Функция установки адреса контракта проверки условия (может менять только владелец)
     function setConditionChecker(address _checker) external onlyOwner {
         conditionChecker = _checker;
     }
 
-    // Функция выпуска токенов на указанный адрес, если выполняется условие
     function mintTokens(address to, uint256 amount) external onlyOwner {
         require(totalSupply() + amount <= maxSupply, "Exceeds max supply");
         require(_checkCondition(), "Condition not met");
@@ -39,12 +37,10 @@ contract TokenWithCustomMinting is ERC20, Ownable {
         emit TokensMinted(to, amount);
     }
 
-    // Функция для изменения цены токенов (может только владелец)
     function setTokenPrice(uint256 _price) external onlyOwner {
         tokenPrice = _price;
     }
 
-    // Вспомогательная функция проверки внешнего условия
     function _checkCondition() internal view returns (bool) {
         require(conditionChecker != address(0), "Condition checker not set");
         (bool success, bytes memory data) = conditionChecker.staticcall(
@@ -54,7 +50,6 @@ contract TokenWithCustomMinting is ERC20, Ownable {
         return abi.decode(data, (bool));
     }
 
-    // Функция покупки токенов пользователем
     function buyTokens() external payable {
         uint256 amount = msg.value / tokenPrice;
         require(amount > 0, "Insufficient ETH sent");
@@ -64,7 +59,6 @@ contract TokenWithCustomMinting is ERC20, Ownable {
         emit TokensMinted(msg.sender, amount);
     }
 
-    // Вывод ETH из контракта (только владелец)
     function withdraw() external onlyOwner {
         payable(owner()).transfer(address(this).balance);
     }
